f899395b33f7de9b8b221369b5146eb7c641d508
unknown <astolfoandresromero@gmail.com>
2023-08-11 22:39:06 -0400

    cambio en imagen y se agrego logica

--- main.js
+++ main.js
@@ -31,3 +31,28 @@ function validateForm(){
 
     return true;
 }
+
+//cree una variable para comunicarnos con el local storage, cree la lista y los botones para editar y/o eliminar al usuario.
+// no coloque la informacion en el cuadro para que no fuese tan grande el cuadro pero si aparece al seleccionar los datos del usuario.
+function ReadData(){
+    let listPeople;
+
+    if (localStorage.getItem('listPeople') == null) {
+        listPeople = [];
+    }else{
+        listPeople = JSON.parse(localStorage.getItem('listPeople'));
+    }
+
+    let html = "";
+
+    listPeople.forEach(function(element, index) {
+        html += "<tr>";
+        html += "<td>" + element.email + "</td>";
+        html += "<td>" + element.name + "</td>";
+        html += "<td>" + element.phone + "</td>";
+        html += '<td><button onclick="deleteData('+ index +')" class="btn btn-danger">Eliminar Dato</button> <button onclick="editData('+ index +')" class="btn btn-warning">Editar Dato</button>';
+        html += "</tr>";
+    });
+
+    document.querySelector('#tableData').innerHTML = html;
+}
